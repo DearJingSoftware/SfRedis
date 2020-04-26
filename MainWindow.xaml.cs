61104c1a952488af380fab68c0859f84511eb67c
jingdor <945627077@qq.com>
2020-04-26 19:36:24 +0800

    更新

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Win32;
+﻿
+using SfRedis.Sessions;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
+using System.Windows.Forms;
 
 namespace SfRedis
 {
@@ -17,10 +17,10 @@ namespace SfRedis
     /// </summary>
     public partial class MainWindow : Window
     {
-      
+
         class TextWriter1 : TextWriter
         {
-            internal TextBox textBlock;
+            internal System.Windows.Controls.TextBox textBlock;
 
             public override Encoding Encoding => throw new NotImplementedException();
             public override void WriteLine(string value)
@@ -36,7 +36,8 @@ namespace SfRedis
             }
         }
 
-       
+        public static ObservableCollection<Session> sessions = new ObservableCollection<Session> { };
+
         ConnectionMultiplexer redis = null;
 
         IDatabase db = null;
@@ -45,20 +46,16 @@ namespace SfRedis
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
 
-            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Sfigure", "Session", "hi Sfigure");
-
-            string Session = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Sfigure", "Session", null);
-            if (Session != null)
-            {
-                Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Sfigure", "Session", "hi Sfigure");
-            }
             RedisHostConnect.IsEnabled = true;
             RedisHostDisconnect.IsEnabled = false;
+            this.SessionTree.ItemsSource = sessions;
+
         }
         /// <summary>
         /// 断开连接
         /// </summary>
-        private void Button_Redis_Disconnect(object sender, RoutedEventArgs e) {
+        private void Button_Redis_Disconnect(object sender, RoutedEventArgs e)
+        {
             RedisHostConnect.IsEnabled = true;
             RedisHostDisconnect.IsEnabled = false;
             redis.CloseAsync();
@@ -68,81 +65,108 @@ namespace SfRedis
         {
             TreeViewItem target = (TreeViewItem)sender;
             String key = target.Header.ToString();
-            string[] a = { key };
-            RedisResult redisResult= db.Execute("get", a );
-            parseResult(redisResult);
+            RedisResult redisResult = FetchKey(key);
+            ParseResult(redisResult);
         }
-        private void  SessionTreeItemMouseDoubleClick(object sender, RoutedEventArgs e) {
-            MessageBox.Show("双击");
+
+        private RedisResult FetchKey(string key)
+        {
+
+            RedisType redisKeyTypeResult = db.KeyType(new RedisKey(key));
+            String fetch = null;
+            List<string> para = new List<string>();
+            switch (redisKeyTypeResult)
+            {
+                case RedisType.None:
+                    fetch = null;
+                    break;
+                case RedisType.String:
+                    fetch = "GET";
+                    para.Add(key);
+                    break;
+                case RedisType.Set:
+                    fetch = "SMEMBERS";
+                    para.Add(key);
+                    break;
+                case RedisType.Hash:
+                    fetch = "HGETALL";
+                    //MessageBox.Show("HGETALL");
+                    para.Add(key);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            RedisResult redisResult = db.Execute(fetch, para.ToArray());
+            return redisResult;
+        }
+
+        private void SessionTreeItemMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("双击");
         }
         /// <summary>
         /// 连接
         /// </summary>
         private void Button_Redis_Connect(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                if (redis.IsConnecting)
-                {
-                    redis.Dispose();
-                }
-            }
-            catch { 
-            }
-            //重新初始化
-            try {
-                var log = new MainWindow.TextWriter1();
-                log.textBlock = SessionLog;
-                ConfigurationOptions configurationOptions = new ConfigurationOptions();
-                configurationOptions.ReconnectRetryPolicy= new LinearRetry(5000);
-
-                
-                redis = ConnectionMultiplexer.Connect(RedisHost.Text, log);
-                db = redis.GetDatabase();
-                RedisHostConnect.IsEnabled = false;
-                RedisHostDisconnect.IsEnabled = true;
-
-                TreeViewItem a = new TreeViewItem();
-
-                StackPanel stack = new StackPanel();
-                stack.Orientation = Orientation.Horizontal;
-
-                // create Image
-                Image image = new Image();
-                image.Source = new BitmapImage
-                    (new Uri("pack://application:,,/Images/" + "redis.ico"));
-                image.Width = 16;
-                image.Height = 16;
-                // Label
-                Label lbl = new Label();
-                lbl.Content = RedisHost.Text;
 
 
-                // Add into stack
-                stack.Children.Add(image);
-                stack.Children.Add(lbl);
-                a.Header =stack;
+            //try
+            //{
+            //    var log = new MainWindow.TextWriter1();
+            //    log.textBlock = SessionLog;
+            ConfigurationOptions configurationOptions = new ConfigurationOptions();
+            configurationOptions.ReconnectRetryPolicy = new LinearRetry(5000);
 
-                //增加
-                //获取全部的key
-                string[] argv = { "*" };
-                var redisResult = db.Execute("keys", argv);
 
-                foreach (KeyValuePair<string, RedisResult> entry in redisResult.ToDictionary())
-                {
-                   
-                    TreeViewItem KeyItem = new TreeViewItem();
-                    KeyItem.Header= entry.Value;
-                    KeyItem.MouseDoubleClick += KeyItemMouseDoubleClick;
-                    a.Items.Add(KeyItem);
-                }
-                SessionTree.Items.Add(a);
-            }
-            catch (Exception ex) {
-                SessionLog.Text = ex.Message.ToString();
-            }
-          
-        
+            redis = ConnectionMultiplexer.Connect(RedisHost.Text);
+            db = redis.GetDatabase();
+            RedisHostConnect.IsEnabled = false;
+            RedisHostDisconnect.IsEnabled = true;
+
+            //    TreeViewItem a = new TreeViewItem();
+
+            //    StackPanel stack = new StackPanel();
+            //    stack.Orientation = Orientation.Horizontal;
+
+            //    // create Image
+            //    Image image = new Image();
+            //    image.Source = new BitmapImage
+            //        (new Uri("pack://application:,,/Images/redis.ico"));
+            //    image.Width = 16;
+            //    image.Height = 16;
+            //    // Label
+            //    Label lbl = new Label();
+            //    lbl.Content = RedisHost.Text;
+
+
+            //    // Add into stack
+            //    stack.Children.Add(image);
+            //    stack.Children.Add(lbl);
+            //    a.Header = stack;
+
+            //    //增加
+            //    //获取全部的key
+            //    string[] argv = { "*" };
+            //    var redisResult = db.Execute("keys", argv);
+
+            //    foreach (KeyValuePair<string, RedisResult> entry in redisResult.ToDictionary())
+            //    {
+
+            //        TreeViewItem KeyItem = new TreeViewItem();
+            //        KeyItem.Header = entry.Value;
+            //        KeyItem.MouseDoubleClick += KeyItemMouseDoubleClick;
+            //        a.Items.Add(KeyItem);
+            //    }
+            //    SessionTree.Items.Add(a);
+            //}
+            //catch (Exception ex)
+            //{
+            //    SessionLog.Text = ex.Message.ToString();
+            //}
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -150,7 +174,7 @@ namespace SfRedis
 
         }
 
-        public static String[] SubArray<String>(String[]  data, int index, int length)
+        public static String[] SubArray<String>(String[] data, int index, int length)
         {
             String[] result = new String[length];
             Array.Copy(data, index, result, 0, length);
@@ -164,12 +188,13 @@ namespace SfRedis
 
         private void RedisCommandExec_Click(object sender, RoutedEventArgs e)
         {
-           
+
 
         }
 
-        private  void parseResult(RedisResult redisResult) {
-            RedisResult.Text = redisResult.Type.ToString();
+        private void ParseResult(RedisResult redisResult)
+        {
+            RedisResult.Text = "";
             switch (redisResult.Type)
             {
                 case ResultType.BulkString:
@@ -186,8 +211,9 @@ namespace SfRedis
                     {
                         foreach (KeyValuePair<string, RedisResult> entry in redisResult.ToDictionary())
                         {
-                            RedisResult.Text = RedisResult.Text + "\n" + entry.Value;
+                            RedisResult.Text = RedisResult.Text + "\n" + entry.Key + ":" + entry.Value;
                         }
+
                         break;
                     }
                 case ResultType.Integer:
@@ -210,20 +236,33 @@ namespace SfRedis
 
         private void RedisCommandExec_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == System.Windows.Input.Key.Return) {
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
                 try
                 {
                     string[] args = RedisCommandInput.Text.Split(' ');
                     string[] arg1 = SubArray(args, 1, args.Length - 1);
                     RedisResult redisResult = db.Execute(args[0], arg1);
-                    parseResult(redisResult);
-                   
+                    ParseResult(redisResult);
+
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     SessionLog.Text = ex.Message.ToString();
                 }
             }
+        }
+
+        private void Button_Redis_New(object sender, RoutedEventArgs e)
+        {
+
+            var redission = new RedisSession();
+            redission.Create();
+        }
+
+        private void TreeViewItem_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ((SfRedis.Sessions.RedisSession)(((TreeViewItem)sender).DataContext)).Connect();
         }
     }
 }
